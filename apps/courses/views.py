658624aefcc7e36d20f3dc2ca3c301def7cb6547
Sebastian HO <sebastianhodev@gmail.com>
2026-01-16 18:41:47 -0600

    courses templates, urls and views

--- apps/courses/views.py
+++ apps/courses/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def course_list(request):
+    return render(request, 'courses/courses.html')
+
+def course_detail(request):
+    return render(request, 'courses/course_detail.html')
+
+def course_lessons(request):
+    return render(request, 'courses/course_lessons.html')
--- ilearning/settings.py
+++ ilearning/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'apps.courses',
+    'apps.profiles',
+    'apps.dashboard',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
