8ec710575e1058f7411da18aff0f50ea611c28ef
Sebastian HO <sebastianhodev@gmail.com>
2026-01-19 12:17:54 -0600

    dahsboard template, fix cusmtom styles

--- ilearning/urls.py
+++ ilearning/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('courses/', include('apps.courses.urls')),
+    path('dashboard/', include('apps.dashboard.urls')),
     # path('profile/', include('apps.profiles.urls')),
-    # path('dashboard/', include('apps.dashboard.urls')),
 ]
