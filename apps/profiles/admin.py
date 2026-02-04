027e89ffede75eb39abce321188e3b29c93f1d17
Sebastian HO <sebastianhodev@gmail.com>
2026-02-03 19:21:52 -0600

    register user and instructorprofile admin

--- apps/profiles/admin.py
+++ apps/profiles/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User, InstructorProfile
 
 # Register your models here.
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    fieldsets = BaseUserAdmin.fieldsets + (
+        ("Rol personalizado",{"fields": ("is_instructor",)},),
+    )
+    add_fieldsets = BaseUserAdmin.add_fieldsets + (
+        (None,{"fields": ("is_instructor",)},),
+    )
+    list_display = ['username', 'email', 'first_name', 'last_name', 'is_instructor', 'is_superuser', 'is_staff', 'is_active']
+
+
+admin.site.register(InstructorProfile)
