65b810b3b06c01c410d141801fb343f10794ecd4
Sebastian HO <sebastianhodev@gmail.com>
2026-02-06 16:03:46 -0600

    searching tool in course list

--- apps/courses/views.py
+++ apps/courses/views.py
@@ -1,51 +1,25 @@
+import logging
 from django.shortcuts import render
 from .models import Course
+from django.db.models import Q
 
 # Create your views here.
+logger = logging.getLogger(__name__)
 
 
 def course_list(request):
     courses = Course.objects.all()
-    # courses = [
-    #     {
-    #         'id': 1,
-    #         'level': 'Principiante',
-    #         'rating': 4.8,
-    #         'course_title': 'Python: fundamentos hasta los detalles',
-    #         'instructor': 'Alison Walsh',
-    #         'course_image': 'images/curso_1.jpg',
-    #         'instructor_image': 'https://randomuser.me/api/portraits/women/68.jpg',
-    #     },
-    #     {
-    #         'id': 2,
-    #         'level': 'Intermedio',
-    #         'rating': 4.9,
-    #         'course_title': 'Django: crea aplicaciones Robustas',
-    #         'instructor': 'Patty Kutch',
-    #         'course_image': 'images/curso_2.jpg',
-    #         'instructor_image': 'https://randomuser.me/api/portraits/women/20.jpg',
-    #     },
-    #     {
-    #         'id': 3,
-    #         'level': 'Avanzado',
-    #         'rating': 4.8,
-    #         'course_title': 'Django Advanced Level Course',
-    #         'instructor': 'Alonzo Murray',
-    #         'course_image': 'images/curso_3.jpg',
-    #         'instructor_image': 'https://randomuser.me/api/portraits/men/32.jpg',
-    #     },
-    #     {
-    #         'id': 4,
-    #         'level': 'Avanzado',
-    #         'rating': 4.8,
-    #         'course_title': 'FastApi: Building APIs with Python and FastAPI',
-    #         'instructor': 'Gregory Harris',
-    #         'course_image': 'images/curso_4.jpg',
-    #         'instructor_image': 'https://randomuser.me/api/portraits/men/45.jpg',
-    #     },
-    # ]
     
-    return render(request, 'courses/courses.html', {'courses': courses})
+    query = request.GET.get('query').strip()
+    if query:
+        logger.info(f"Buscando cursos con el query: {query}")
+        courses = courses.filter(
+            Q(title__icontains=query) | 
+            Q(owner__first_name__icontains=query) |
+            Q(owner__last_name__icontains=query)
+        )
+    
+    return render(request, 'courses/courses.html', {'courses': courses, 'query': query})
 
 def course_detail(request):
     course = {
